a1345ac55073911f8b90c41779a60d9af81795ca
cpinkz-april <cp.aethaya@gmail.com>
2025-06-12 07:17:40 +0700

    Create mad_lips.py

---
+++ Mad Libs/mad_lips.py
@@ -0,0 +1,14 @@
+restaurant = input("Enter name of restaurant: ")
+adjective, adjective2, adjective3, adjective4, adjective5, adjective6 = input("Enter adjective: ")
+noun, noun2 = input("Enter noun: ")
+verb = input("Enter verb: ")
+food = input("Enter food: ")
+body_part = input("Enter body part: ")
+clothing = input("Enter clothing: ")
+dessert = input("Enter dessert: ")
+color = input("Enter color: ")
+
+print("Last night I went to " + restaurant + " for dinner. The food was " + adjective + " and " + adjective2 + " , but I enjoyed the " + noun + " . Our waitress was kind of " + adjective3
+      + " because she kept on " + verb + " and she got our oder " + adjective4 + " . She also dropped " + food + " on my mom's " + body_part + " . My mom was really " + adjective5 + 
+      " because it ruined her " + clothing + " For dessert, we got a " + dessert + " that had " + color + " frosting. It was pretty good but when I was chewing a find a " + 
+            noun2 + " in the middle. All in all, it was a " + adjective6 + " restaurant experience.")
